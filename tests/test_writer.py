5294a10c586ae79d9060fcaa71eb5af9c45c3fd1
Evan Dort <edort@wasatchphotonics.onmicrosoft.com>
2022-06-09 09:51:15 -0400

    add test to check some of the header fields

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -13,6 +13,8 @@ sys.path.append(os.path.join(os.getcwd(),"src")) # Adds higher directory to pyth
 from SPyC_Writer.SPCFileWriter import SPCFileWriter
 from SPyC_Writer.SPCEnums import SPCFileType
 
+from SPyC_Writer.common import RES_DESC_LIMIT, SRC_INSTRUMENT_LIMIT
+
 
 # from hyptohesis docs. Allows for multidim lists with same length
 # https://hypothesis.readthedocs.io/en/latest/data.html
@@ -96,10 +98,49 @@ class TestWritingParse:
     @settings(deadline=None, max_examples=30)
     def test_yyy_format(self, y_values: list[float]) -> None:
         """
-        File type is tested even though KIA lists invalid while others parse successfully
+        File type is tested even though KIA reports invalid while others parse successfully
         """
         writer = SPCFileWriter(SPCFileType.TMULTI)
         y_values = np.array(y_values)
         file_output = os.path.join(self.OUTPUT_DIR, f"spc_yyy_output{datetime.datetime.now().strftime('%H_%M_%S_%f')}.spc")
         writer.write_spc_file(file_output, y_values=y_values)
         assert self.comapre_spc_file_array(file_output, y_values), "yyy format spc output array doesn't match parse"
+
+    @given(st.lists(st.floats(width=16, allow_nan=False), min_size=2, max_size=30),
+           st.integers(min_value=0, max_value=31),
+           st.integers(min_value=0, max_value=30),
+           st.integers(min_value=0, max_value=31),
+           # hypothesis generates all sorts of unicode, which we expect to be invalid
+           # check cases that should always be valid by generating ASCII
+           st.text(alphabet=st.characters(min_codepoint = 32, max_codepoint = 126), max_size = RES_DESC_LIMIT),
+           st.text(alphabet=st.characters(min_codepoint = 32, max_codepoint = 126), max_size = SRC_INSTRUMENT_LIMIT),
+           )
+    @settings(deadline=None, max_examples=30)
+    def test_header_data(self,
+                         y_values: list[float],
+                         x_units: int,
+                         y_units: int,
+                         z_units: int,
+                         res_desc: str,
+                         src_instrument_desc: str,
+                         ) -> None:
+        time = datetime.datetime.now()
+        writer = SPCFileWriter(
+            SPCFileType.DEFAULT,
+            compress_date = time,
+            x_units = x_units,
+            y_units = y_units,
+            z_units = z_units,
+            res_desc = res_desc,
+            src_instrument_desc = src_instrument_desc,
+            )
+        y_values = np.asarray(y_values)
+        file_output = os.path.join(self.OUTPUT_DIR, f"spc_y_output{datetime.datetime.now().strftime('%H_%M_%S_%f')}.spc")
+        writer.write_spc_file(file_output, y_values=y_values)
+        data = spc.File(file_output)
+        comparison_inputs = [time.year, time.month, time.day, x_units, y_units, z_units, res_desc, src_instrument_desc]
+        comparison_outputs = [data.year, data.month, data.day, data.fxtype, data.fytype, data.fztype, data.fres.decode("utf-8").replace("\x00",""), data.fsource.decode("utf-8").replace("\x00","")]
+        comparison = [inputs == outputs for inputs, outputs in zip(comparison_inputs, comparison_outputs)]
+        log.debug(f"input data {comparison_inputs}\n and output data {comparison_outputs}\nheaders didn't match")
+        assert all(comparison), f"input data {comparison_inputs}\n and output data {comparison_outputs}\nheaders didn't match"
+
