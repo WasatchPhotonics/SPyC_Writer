0f2f9adb0d3722768f029b6e50698ea5d456995f
rc14193 <62862738+rc14193@users.noreply.github.com>
2025-09-08 20:58:57 -0400

    add module import to package

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .src.SPyC_Writer.SPCDate import *
+from .src.SPyC_Writer.SPCEnums import *
+from .src.SPyC_Writer.SPCFileWriter import *
+from .src.SPyC_Writer.SPCHeader import *
+from .src.SPyC_Writer.SPCLog import *
+from .src.SPyC_Writer.SPCSubheader import *
