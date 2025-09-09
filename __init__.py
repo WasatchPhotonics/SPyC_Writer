e48d8001ef56b09f65aae5180ed107fa8d77da89
rc14193 <62862738+rc14193@users.noreply.github.com>
2025-09-08 21:28:12 -0400

    move init.py, update toml

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .src.SPyC_Writer.SPCDate import *
-from .src.SPyC_Writer.SPCEnums import *
-from .src.SPyC_Writer.SPCFileWriter import *
-from .src.SPyC_Writer.SPCHeader import *
-from .src.SPyC_Writer.SPCLog import *
-from .src.SPyC_Writer.SPCSubheader import *
